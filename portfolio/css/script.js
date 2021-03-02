a0af70413ecdd7eee3583587f622f2e9dd4b6352
Jake Allen <jake.allen@me.com>
2021-03-02 11:24:47 -0500

    add java and contact page

---
+++ portfolio/css/script.js
@@ -0,0 +1,14 @@
+let imageCount = 0;
+const boxImage = document.getElementById("boxImage");
+var imageBank = ["./portfolio/images/cat.jpeg","./portfolio/images/shire.jpeg","./portfolio/images/snow.jpeg","./portfolio/images/guitars.jpeg"]
+
+setInterval(changeImage, 5000);
+
+function changeImage() {
+        boxImage.src = imageBank[imageCount];
+        if (imageCount == 3) {
+            imageCount = 0;
+        } else {
+            imageCount++;
+        }
+    }
