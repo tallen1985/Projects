d779d14a9194f67fe08b3cdaffdcbd858704d4e7
Jake Allen <jake.allen@me.com>
2021-03-05 16:12:55 -0500

    gallery experiment

--- portfolio/css/galleryscript.js
+++ portfolio/css/galleryscript.js
@@ -1,24 +1,9 @@
-const galleryImages = ["./images/cat.jpeg", "./images/guitars.jpeg", "./images/shire.jpeg", "./images/snow.jpeg"]
 
-const image1 = document.getElementById("img1");
-const image2 = document.getElementById('img2');
-const image3 = document.getElementById('img3');
-const image4 = document.getElementById('img4');
-const bigpic = document.getElementById('bigPicImg');
+let bigpic = document.getElementById('bigPicImg');
+let images = document.querySelectorAll('.galleryThumb');
 
-function galleryImage(number){
-    bigpic.src = galleryImages[number];
-}
-
-image1.addEventListener('click', function() {
-     galleryImage(0);
-    });
-image2.addEventListener('click', function() {
-    galleryImage(1);
-   });
-image3.addEventListener('click', function() {
-    galleryImage(2);
-   });
-image4.addEventListener('click', function() {
-    galleryImage(3);
-   });
+images.forEach(function(i) {
+    i.addEventListener('click', function() {
+        bigpic.src = i.src;
+    })
+})
