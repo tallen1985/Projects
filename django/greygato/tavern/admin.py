d14566c817e0a5e1f4276ef1605cdf9a0a629abe
Troy Allen <jake.allen@me.com>
2021-05-28 18:37:23 -0400

    daily

--- django/greygato/tavern/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Employee, Menu, Menu_item
-
-admin.site.register(Employee)
-admin.site.register(Menu)
-admin.site.register(Menu_item)
--- django/greygato/tavern/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name="home"),
-    path('staff', views.staff, name="staff"),
-    path('about', views.about, name='about'),
-    path('menu', views.menu, name="menu")
-]
