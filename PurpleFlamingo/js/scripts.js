bc4dc660e3a2c39294772230db02b00edfaf6d09
Troy Allen <jake.allen@me.com>
2021-05-31 17:00:57 -0400

    add jquery

--- PurpleFlamingo/js/scripts.js
+++ PurpleFlamingo/js/scripts.js
@@ -1,3 +1,27 @@
-$(document).ready(()=>{
-    
+$(document).ready(() => {
+    $('#link-home').on('click', () => {
+        $('#home').show();
+        $('#products').hide();
+        $('#about').hide();
+        $('#contact').hide();
+    })
+    $('#link-products').on('click', () => {
+        $('#home').hide();
+        $('#products').show();
+        $('#about').hide();
+        $('#contact').hide();
+    })
+    $('#link-about').on('click', () => {
+        $('#home').hide();
+        $('#products').hide();
+        $('#about').show();
+        $('#contact').hide();
+    })
+    $('#link-contact').on('click', () => {
+        $('#home').hide();
+        $('#products').hide();
+        $('#about').hide();
+        $('#contact').show();
+    })
+
 });
