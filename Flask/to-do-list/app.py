9c10cff97208294c838ff831d20b157c56c68b86
Troy Allen <jake.allen@me.com>
2021-06-23 21:44:09 -0400

    working on it.

--- Flask/to-do-list/app.py
+++ Flask/to-do-list/app.py
@@ -1,1 +1,12 @@
-from Flask import flask
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/<name>')
+def home(name=None):
+    return render_template('index.html', name=name)
+
+
+if __name__ == '__main__':
+    app.run(debug = True)
