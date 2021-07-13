a6b43a80304b931042b55d37f2468b9254e7ec2f
Troy Allen <jake.allen@me.com>
2021-07-13 13:20:02 -0400

    goofy

--- GGP/js/scripts.js
+++ GGP/js/scripts.js
@@ -1,5 +1,13 @@
-$(document).ready(()=>{
-    $('.drop').on('click', () => {
-        $('#myDropdown').toggle();
-    })
+var blur = false;
+$('.showButton').on('click', () => {
+        if (blur == true) {
+            $('#goofyImage').css('filter', '')
+            $('.stuffAbout').css('visibility', 'hidden')
+            blur = false;
+        } else {
+            blur = true;
+            $('.stuffAbout').css('visibility', 'visible')
+            $('#goofyImage').css('filter', 'blur(4px)')
+        };
+    
 })
